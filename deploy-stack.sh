b7ecf3c8f485f801660d075d81806303ce83c76f
Javviviii2 <jrofe96@gmail.com>
2023-05-09 13:42:22 +0200

    ejercicio8 filtradp

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -1,5 +1,22 @@
-
-
 aws cloudformation deploy \
     --template-file ./main.yml \
-    --stack-name ampliando-template \
+    --stack-name Filtrado-salida 
+
+#comprobar que se ejecute correctamente el deploy
+# $? variable de nivel de error
+# -eq = igual
+# fi =>cerrar if
+if [ $? -eq 0 ] 
+then
+#comando para exportar salida en formato json
+#se ejecuta después de la creación si es correcto
+    aws cloudformation list-exports \
+        --output json \
+# ? 
+        --query "Exports[?Name=='ServerPublicIP'].Value" >list-export-filtered.json
+    echo "ejecución realizada con éxito" 
+    echo " ٩(˘◡˘)۶ "
+else 
+    echo "error"
+    echo " (╯°□°）╯︵ ┻━┻ "
+fi
