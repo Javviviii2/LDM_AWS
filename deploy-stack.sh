84d3037e0fedfb43d9116a381672071a5ba3a95d
Javviviii2 <jrofe96@gmail.com>
2023-05-19 20:43:09 +0200

    .

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -1,1 +1,2 @@
-aws cloudformation deploy --template-file main.yml --stack-name Pila-Javi
+typeinstance=t2.small
+aws cloudformation deploy --template-file main.yml --stack-name Pila-Javi --parameter-overrides InstanceType=$typeinstance
